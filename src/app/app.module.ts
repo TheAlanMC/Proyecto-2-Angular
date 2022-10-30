9bc8e1a337780ec631214f0e57ff0a90b2f83f0a
Chris Apaza <chrisalanapazaa@gmail.com>
2022-10-30 18:26:20 -0400

    added home page and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,59 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Angular Material
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const MaterialComponents = [
+  MatToolbarModule,
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule,
+  MatIconModule,
+  MatTableModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSnackBarModule
+]
+
+// Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PageHomeComponent } from './page-home/page-home.component';
+import { PageCatalogComponent } from './page-catalog/page-catalog.component';
+import { PageFormComponent } from './page-form/page-form.component';
+import { PageContactUsComponent } from './page-contact-us/page-contact-us.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PageHomeComponent,
+    PageCatalogComponent,
+    PageContactUsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    [MaterialComponents]
   ],
-  providers: [],
+  exports: [
+    [MaterialComponents]
+  ], providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
