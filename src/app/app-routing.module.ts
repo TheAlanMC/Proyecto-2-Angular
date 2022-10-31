1e7c13036d5bc7e2d721705b5bb765005a9bc701
Chris Apaza <chrisalanapazaa@gmail.com>
2022-10-30 20:02:05 -0400

    finished home-page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageHomeComponent } from './page-home/page-home.component';
+import { PageCatalogComponent } from './page-catalog/page-catalog.component';
+import { PageFormComponent } from './page-form/page-form.component';
+import { PageContactUsComponent } from './page-contact-us/page-contact-us.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'home',
+    component: PageHomeComponent
+  },
+  {
+    path: '',
+    component: PageHomeComponent
+  },
+  {
+    path: 'catalogo',
+    component: PageCatalogComponent
+  },
+  {
+    path: 'compra',
+    component: PageFormComponent
+  },
+  {
+    path: 'contactanos',
+    component: PageContactUsComponent
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
